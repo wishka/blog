16c87abef8c2691b60a64b64ba82ee6f25fe86a3
wishka <roucke@mail.ru>
2020-04-22 17:02:29 +0300

    authenticate user for new, create only

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,9 @@
 class ApplicationController < ActionController::Base
-  before_action :configure_permitted_parameters, if: devise_controller?
+  before_filter :configure_permitted_parameters, if: :devise_controller?
 
   private
 
-  def onfigure_permitted_parameters
+  def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up) << :username
   end
 end
--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,10 +1,12 @@
 class CommentsController < ApplicationController
+  before_action :authenticate_user!, :only => [:new, :create]
+  
   def create
     @article = Article.find(params[:article_id])
     @article.comments.create(comment_params)
 
     redirect_to article_path(@article)
-    
+
   end
 
   private
