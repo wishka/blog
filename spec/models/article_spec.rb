7f7459168b48acbf8f339604f5ff3b9e9c7d2906
wishka <roucke@mail.ru>
2020-04-23 22:19:18 +0300

    add validation to article add article spec (dont work)

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,5 +1,13 @@
 require "spec_helper"
 
 RSpec.describe Article, type: :model do
-  it { should have_many :comments }
+  describe 'validations' do
+    it { is_expected.to validate_presence_of (:title) }
+    it { is_expected.to validate_presence_of (:text) }
+end
+
+  RSpec.describe Article, type: :model do
+      describe 'associations' do
+      it { should have_many :comments }
+      end
 end
