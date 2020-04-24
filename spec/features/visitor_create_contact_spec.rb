5ec5ed4b37cf4481eab4c75d021a9d29c79c7ee9
wishka <roucke@mail.ru>
2020-04-24 12:50:55 +0300

    add page Contact

--- spec/features/visitor_create_contact_spec.rb
+++ spec/features/visitor_create_contact_spec.rb
@@ -3,7 +3,7 @@ require "spec_helper"
 feature "Contact Creation" do
 scenario "allows access to contacts page" do
   visit '/contacts'
-  expect(page).to have_contact 'Contact us'
+  expect(page).to have_contact I18n.t ('contact.contact_us')
 
 end
 end
