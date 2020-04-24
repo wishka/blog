556a8e16224bd20ff4b9a3262315284ea339124a
wishka <roucke@mail.ru>
2020-04-24 13:51:03 +0300

    update test feature

--- spec/features/visitor_create_contact_spec.rb
+++ spec/features/visitor_create_contact_spec.rb
@@ -4,6 +4,14 @@ feature "Contact Creation" do
 scenario "allows access to contacts page" do
   visit '/contacts'
   expect(page).to have_contact I18n.t ('contact.contact_us')
+  end
 
-end
+  scenario "allows a guest to create contact" do
+    visit '/contacts'
+    fill_in :contact_email, :with => 'user@example.com'
+    fill_in :contact_message, :with => 'something'
+    click_button 'Send message'
+
+    expect(page).to have_content 'Thanks!'
+  end
 end
